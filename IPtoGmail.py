131742223cfed63bf51b102cfd050934d920ba1f
Javier Martínez <javimtmz96@gmail.com>
2021-09-13 22:25:01 +0200

    Fix as_bytes and directory

--- IPtoGmail.py
+++ IPtoGmail.py
@@ -31,7 +31,7 @@ def create_message(sender, to, subject, message_text):
     message['to'] = to
     message['from'] = sender
     message['subject'] = subject
-    encoded_message = urlsafe_b64encode(message.as_bytes())
+    encoded_message = urlsafe_b64encode(message.as_string())
     return {'raw': encoded_message.decode()}
 
 
@@ -60,8 +60,8 @@ def get_ip():
     ips = {}
     ips['new'] = "new"
     ips['old'] = "old"
-    if os.path.exists('/ips.json'):
-        with open('/ips.json') as f:
+    if os.path.exists('./ips.json'):
+        with open('./ips.json') as f:
             ips = json.load(f)
             response = requests.get("https://api.ipify.org?format=json")
             resApi = response.json()
@@ -72,11 +72,11 @@ def get_ip():
                 print("Nueva ip detectada, actualizando..")
                 ips['old'] = ips['new']
                 ips['new'] = resApi['ip']
-                with open('/ips.json', 'w') as json_file:
+                with open('./ips.json', 'w') as json_file:
                     json.dump(ips, json_file)
                 return resApi['ip']
     else:
-        with open('/ips.json', 'w') as outfile:
+        with open('./ips.json', 'w') as outfile:
             json.dump(ips, outfile)
         return 0
 
@@ -89,18 +89,18 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('/token.json'):
-        creds = Credentials.from_authorized_user_file('/token.json', SCOPES)
+    if os.path.exists('./token.json'):
+        creds = Credentials.from_authorized_user_file('./token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                '/credentials.json', SCOPES)
+                './credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('/token.json', 'w') as token:
+        with open('./token.json', 'w') as token:
             token.write(creds.to_json())
 
     service = build('gmail', 'v1', credentials=creds)
